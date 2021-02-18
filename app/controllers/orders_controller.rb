3437c80bac098738353de8443ba911d45dbdf9e8
couplee <couplee888@gmail.com>
2021-02-18 12:03:32 +0000

    [Add] order-products' qit quit wq! :wq! : q

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   
   def new
+    @order = Order.new
   end
 
   def index
@@ -10,12 +11,21 @@ class OrdersController < ApplicationController
   end
   
   def create
+    @item = Item.find(params[:item_id])
+    @order = @product.order.new(order_params)
+    @order.save
+    redirect_to thanks_customer_orders_path
   end
   
   def thanks
   end
 
   def show
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:amount, :quantity, :item_id)
   end
   
 end
