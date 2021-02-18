48bd038b024ac534cfccb25202a8e9cdd5c635dd
EC2 Default User <08g0531mkm@gmail.com>
2021-02-18 10:41:45 +0000

    会員情報が表示されない部分の修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ # validates :family_name, :first_name, :family_name_kana, :first_name_kana, :postcode, :address, :phone_number, presence: true
+  #validates :is_deleted, inclusion:{in: [true, false]}
 end
