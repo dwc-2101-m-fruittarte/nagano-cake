3ec5cc9f130a689159e01cc7a1591442ad749035
couplee <couplee888@gmail.com>
2021-02-20 01:34:23 +0000

    [Add] a little bit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,13 +8,13 @@ class OrdersController < ApplicationController
   end
 
   def info
-    @orders = current_customer.orders
-    
+   @order = Order.new
+   @cart_items = current_customer.cart_items
   end
 
   def create
     @product = Product.find(params[:product_id])
-    @order = @product.order.new(order_params)
+    @order = Order.new(order_params)
     if @order.save
       redirect_to thanks_orders_path
     else
@@ -26,5 +26,13 @@ class OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_products = @order.order_products
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :name, :postcode, :address, :bill, :shipping, :payment_method, :status)
+  end
+
+end
