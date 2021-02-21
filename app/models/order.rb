e949cff9efdbc0d3cb77e769e4e87714e5ad66b1
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-02-21 10:02:49 +0000

    管理者・注文履歴一覧

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
+
    has_many :products, through: :order_products
+   has_many :order_products
    belongs_to :customer
 
   enum payment_method: {クレジットカード:0, 銀行振込:1}
