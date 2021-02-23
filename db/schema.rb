de5a5d4c4ebc17f4ecb038e95eda29f6b128a1a9
couplee <couplee888@gmail.com>
2021-02-23 05:06:53 +0000

    途中

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_031952) do
     t.string "postcode"
     t.string "address"
     t.string "phone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
